e1b64f0714d7d284b078f1c5809765ecc90e23e5
Akoruu.Dev <akoruu.dev@gmail.com>
2022-12-14 21:16:15 -0300

    Exercícios JOIN

---
+++ Prática - Exercícios Join/ex02.sql
@@ -0,0 +1,1 @@
+SELECT testimonials.id, writer.name AS writer, recipient.name, testimonials.message FROM testimonials JOIN users as writer ON testimonials."writerId" = writer.id JOIN users as recipient ON testimonials."recipientId" = recipient.id;
